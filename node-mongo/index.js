81bcd3940f59489d246d6c4d93f1b3df4a82a45a
knarsimh <kasu.narsimharaj@capgemini.com>
2020-07-24 15:12:45 +0530

    mongoose ODM

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,36 +1,46 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err, client) => {
-
-    assert.equal(err,null);
+MongoClient.connect(url).then((client) => {
 
     console.log('Connected correctly to server');
-
     const db = client.db(dbname);
-    const collection = db.collection("dishes");
-    collection.insertOne({"name": "Uthappizza", "description": "test"},
-    (err, result) => {
-        assert.equal(err,null);
 
-        console.log("After Insert:\n");
-        console.log(result.ops);
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test" },
+        "dishes")
+        .then((result) => {
+            console.log("Insert Document:\n", result.ops);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
-            
-            console.log("Found:\n");
-            console.log(docs);
+            return dboper.findDocuments(db, "dishes")
+        })
+        .then((docs) => {
+            console.log("Found Documents:\n", docs);
 
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
+            return dboper.updateDocument(db, { name: "Vadonut" },
+                { description: "Updated Test" }, "dishes")
+        })
+        .then((result) => {
+            console.log("Updated Document:\n", result.result);
 
-                client.close();
-            });
+            return dboper.findDocuments(db, "dishes")
+        })
+        .then((docs) => {
+            console.log("Found Updated Documents:\n", docs);
+
+            return db.dropCollection("dishes")
+        })
+        .then((result) => {
+            console.log("Dropped Collection: ", result);
+
+            client.close();
+        }).catch((err) => {
+            console.log(err);
         });
-    });
 
-});
+}).catch(err => {
+    assert.equal(err, null);
+})
---
+++ node-mongo/operations.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+
+exports.insertDocument = (db, document, collection) => {
+    const coll = db.collection(collection);
+    return coll.insert(document);
+};
+
+exports.findDocuments = (db, collection) => {
+    const coll = db.collection(collection);
+    return coll.find({});
+};
+
+exports.removeDocument = (db, document, collection) => {
+    const coll = db.collection(collection);
+    return coll.deleteOne(document);
+};
+
+exports.updateDocument = (db, document, update, collection) => {
+    const coll = db.collection(collection);
+    return coll.updateOne(document, { $set: update }, null);
+};

53e1169c7f3ce897a69c95f977e4a29956380d69
Narsimharaj <kasu.narsimharaj@capgemini.com>
2020-08-25 11:51:32 +0530

    token based authentication

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -12,12 +12,13 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config.js');
 
 const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -34,13 +35,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser("12345-67890-09876-54321"));
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
+// app.use(session({
+//   name: 'session-id',
+//   secret: '12345-67890-09876-54321',
+//   saveUninitialized: false,
+//   resave: false,
+//   store: new FileStore()
+// }));
 
 app.use(passport.initialize);
 app.use(passport.session());
@@ -49,18 +50,18 @@ app.use('/', indexRouter);
 app.use('/users', userRouter);
 
 
-function auth(req, res, next) {
-  console.log(req.session);
+// function auth(req, res, next) {
+//   console.log(req.session);
 
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 401;
-    return next(err);
-  }
-  else {
-      next();
-  }
-}
+//   if (!req.user) {
+//     var err = new Error('You are not authenticated!');
+//     err.status = 401;
+//     return next(err);
+//   }
+//   else {
+//       next();
+//   }
+// }
 
 // function auth(req, res, next) {
 //   console.log(req.session);
@@ -82,7 +83,7 @@ function auth(req, res, next) {
 //   }
 // }
 
-app.use(auth);
+// app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const User = require('../models/user');
 const passport = require('passport');
+const { authenticate } = require('../authenticate');
 
 const router = express.Router();
 router.use(bodyParser.json());
@@ -48,9 +49,10 @@ router.post('/signup', (req, res, next) => {
 })
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+    var token = authenticate.getToken({_id: req.user._id});
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.json({ success: true, status: 'You are successfully logged in!' });
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 // router.post('/login', (req, res, next) => {

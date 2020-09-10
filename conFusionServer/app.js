3ecec1fc4a3b41d9941b555504dca21f47086cea
knarsimharaj <kasu.narsimharaj@discounttire.com>
2020-09-10 14:40:47 +0530

    secure connection using openssl

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -27,14 +27,14 @@ connect.then((db) => {
 
 var app = express();
 
-app.all('*',(req, res, next)=>{
-  if(req.secure){
+app.all('*', (req, res, next) => {
+  if (req.secure) {
     return next();
   }
-  else{
-    res.redirect('https://'+req.hostname+":"+app.get('secPort'));
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
   }
-})
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
